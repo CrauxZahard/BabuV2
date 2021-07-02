c55bc4e96fd6902fdc281c949f967895fff9fead
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-02 03:35:54 -0700

    Create select.js

---
+++ commands/select.js
@@ -0,0 +1,42 @@
+module.exports = {
+  name: 'select',
+  run: async (client, message, args) => {
+    const discord = require('discrod.js');
+    const row = new discord.MessageActionRow()
+			.addComponents(
+				new discord.MessageSelectMenu()
+					.setCustomID('select')
+					.setPlaceholder('Nothing selected')
+					.setMinValues(2)
+					.setMaxValues(3)
+					.addOptions([
+						{
+							label: 'Option 1',
+							description: 'a description of first option',
+							value: 'first_option',
+              emoji: {
+                name: 'SumiBye',
+                id: '844192593516822548',
+                animated: true
+              }
+						},
+						{
+							label: 'Option 2',
+							description: 'a description of second option',
+							value: 'second_option',
+              emoji: {
+                name: 'ngikngok',
+                id: '819960401408163840'
+              }
+						},
+						{
+							label: 'Option 3',
+							description: 'a description of third option',
+							value: 'third_option',
+						},
+					]),
+			);
+    
+    await message.channel.send({content: 'testing', components: [row] })
+  }
+}
