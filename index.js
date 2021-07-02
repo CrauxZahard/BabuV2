b3f454470f20fa2c8440127f4ba22e53d1ec7575
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-02 03:03:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = require('./token.json')
+const token = require('./token.json');
+const { Database } = require('dbdjs.db');
 
-client.once('ready', () => { console.log(`${client.user.tag}`) })
+//database setup
+const db = new Database({
+  path: './databases/',
+  tables: [{name: 'main'}]
+})
+
+db.once('ready', () => { console.log('database is ready') })
+db.connect()
+//end of database setup
+
+client.once('ready', () => { console.log(`${client.user.tag} is ready`) })
+
+//command handler
+client.commands = new Discord.Collection();
+const commandFile = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for(const file of commandFile) {
+  const command = require(`./commands/${file}`)
+  client.commands.set(command.name, command)
+}
+//end of command handler
 
 client.on('message', msg => {
 if(msg.author.bot || !msg.content.startsWith('-')) return;
