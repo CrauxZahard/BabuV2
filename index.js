0c2fad880d585ca2edabd21aab620d9ce42d380e
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-16 18:53:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,8 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
-const token = require('./token.json');
-const { Database } = require('dbdjs.db');
-const fs = require('fs');
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES], partials: ['MESSAGE', 'REACTION', 'CHANNEL'] });
+const { token } = require('./token.json');
 
-//database setup
-const db = new Database({
-  path: './databases/',
-  tables: [{name: 'main'}]
-})
+require('./handler/client.js')(client);
+require('./handler/rpg.js')(client);
 
-db.once('ready', () => { console.log('database is ready') })
-db.connect()
-//end of database setup
-
-client.once('ready', () => { console.log(`${client.user.tag} is ready`) })
-
-//command handler
-client.commands = new Discord.Collection();
-const commandFile = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for(const file of commandFile) {
-  const command = require(`./commands/${file}`)
-  client.commands.set(command.name, command)
-}
-//end of command handler
-
-client.on('message', msg => {
-if(msg.author.bot || !msg.content.startsWith('-')) return;
-  const args = msg.content.slice('-'.length).trim().split(/ +/);
-  const cmd = args.shift().toLowerCase();
-  if(!client.commands.has(cmd)) return;
-  
-  try {
-    //run the command
-    client.commands.get(cmd).run(client, msg, args);
-  }
-  catch (error) {
-    msg.channel.send(':x: an error hass occured!' + `\n${error}`);
-  } 
-  
-})
-
-client.login(token.token)
+client.login(token)
