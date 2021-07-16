aa10b7d6eef9423717a3f260e25431755d138f65
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-16 19:02:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES], partials: ['MESSAGE', 'REACTION', 'CHANNEL'] });
 const { token } = require('./token.json');
 
-require('./handler/client.js')(client);
+require('./handler/events.js')(client);
+require('./handler/file.js')(client);
 require('./handler/rpg.js')(client);
 
 client.login(token)
