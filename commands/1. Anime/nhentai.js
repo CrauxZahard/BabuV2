50429500af64e59c2e9164e456c3e6e6c509a75b
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-28 14:08:43 +0700

    Update nhentai.js

--- commands/1. Anime/nhentai.js
+++ commands/1. Anime/nhentai.js
@@ -1,71 +1,36 @@
 module.exports.name = "nhentai"
 module.exports.aliases = ["doujin", "nh", "search", "s"]
 module.exports.cooldown = 10
+⬅️➡️
 
 module.exports.code = async (client, message, args) => {
-  if (!args) return message.chhannel.send(':x: mau nyari apa?')
-  if (args.length > 1) {
-    //means they are searching a title
-    let raw = await client.hentai.search(args.join(' '))
-    let doujinList = raw.doujins
-    if(!doujinList) return message.channel.send(':x: nggak nemuin judul begitu.')
-    
-    /* message content placeholder
-       will look like smth like this:
-       1.) Emilia raped
-       2.) Emilia fucked
-       3.) Emilia NTR-ed
-    */
-    let placeHolder = []
-    doujinList.forEach((d, n) => {
-    placeHolder.push(`${n+1}). ${d.titles.pretty}\n`)
-    })
-    
-    let pesan = await message.channel.send('pilih angka yg mau dibaca bang:\n' + placeHolder)
-    let messageCollect = await message.channel.createMessageCollector((msg) => msg.author.id == message.author.id, {time: 20000, max: 1})
-    
-    messageCollect.on('collect', async (msg) => {
-     messageCollect.stop()
-     let angka = parseInt(msg.content);
-     if (!angka || angka > doujinList.length) return message.channel.send(':x: angka nggak valid.')
-     pesan.edit('selamat membaca!', {title: doujinList[angka-1].titles.pretty,
-                                     thumbnail: doujinList[angka-1].cover.url,
-                                     image: {url: doujinList[angka-1].pages[0].url}
-                                    })
-      
-      const filter = async (reaction, user) => {
-        if (reaction.emoji.name == '➡️' && user.id == message.author.id) return true
-        if (reaction.emoji.name == '⬅️' && user.id == message.author.id) return true
-        return false
+  let tryNumb = parseInt(args[0])
+  
+  if (tryNumb && args.length > 1) {
+    //searching a title
+    let doujinList = await client.hentai.search(args.join(' '))
+    if (!doujinList) {
+      //no result found
+      return message.channel.send(':x: ga ada judul begitu.')
+    }
+    else if (doujinList.length == 1) {
+      //1 result found
+      let pesan = message.channel.send({content: 'selamat membaca!', embed: {
+        color: 'RANDOM',
+        title: doujinList[0].titlle.pretty,
+        image: {url: doujinList[0].pages[0].url}
       }
-      
-      let reactCollect = await pesan.createReactionCollector(filter, {time: 1000 * 900})
-      let currentPage = 0
-      reactCollect.on('collect', async (r, u) => {
-        if (r.emoji.name == '➡️') {
-          currentPage = currentPage + 1
-          await pesan.edit('selamat membaca!', {title: doujinList[angka-1].titles.pretty,
-                                          thumbnail: doujinList[angka-1].cover.url,
-                                          image: {url: doujinList[angka-1].pages[currentPage].url}
-                                         })
-        }
-        
-        else {
-          currentPage = currentPage - 1
-          await pesan.edit('selamat membaca!', {title: doujinList[angka-1].titles.pretty,
-                                          thumbnail: doujinList[angka-1].cover.url,
-                                          image: {url: doujinList[angka-1].pages[currentPage].url}
-                                          })
-        }
-        
-      })
-      
-    })
+                                       })
+    }
+    else {
+      //multiple result found
+    }
+  }
+  else if (tryNumb && args.length == 1) {
+    //6 digit code
     
   }
-  
-  else { 
-  //means they are using 6-digit number
-    console.log('smth')
+  else {
+    return message.channel.send('pen cari apa?')
   }
 }
