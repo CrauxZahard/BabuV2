48e3854555212b8253a23fee848967528d801bfe
Craux <83466517+CrauxZahard@users.noreply.github.com>
2021-08-05 21:25:02 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,3 @@
 module.exports = client => {
-  client.db.set('dev', ['694573382380945529', '458114349286948864'])
-  const { Collection } = require('discord.js')
-  client.dev.ids = new Collection();
-  
-  client.db.get('dev').forEach(id => {
-    client.dev.ids.set(id, id)
-    console.log('developer ids: ' + client.dev.ids.map(x => x).join(', ') )
-  })
-  
   console.log(`${client.user.tag} is ready`)
 }
