86d51e130820e05370fbaac7636f6d55b861fe78
Craux <83466517+CrauxZahard@users.noreply.github.com>
2021-08-07 18:19:31 +0700

    Update index.js

--- src/items/index.js
+++ src/items/index.js
@@ -8,7 +8,7 @@ class Weapon {
     this.xp = data.xp
     this.owner = data.owner
     this.originalOwner = data.originalOwner
-    this.ownerHistory = data.ownerHistory
+    this.ownerHistory = data.ownerHistory || []
     this.stats = data.stats
     this.skills = data.skill || null
     this.maxSkillSize = data.maxSkillSize || 0
@@ -16,15 +16,37 @@ class Weapon {
   
   upSkillSize(item, database, userWeapon) {
     /*
-    item = weapon class object
+    item = weapon class object OR item id (string)
     database = the database table to set new value
     userWeapon = user weapon collection (array)
     */
+    for(var i = 0;i < userWeapon.length) {
+      if(userWeapon[i].id === item.id || userWeapon[i].id === item) {
+        userWeapon[i].maxSkillSize += 1
+      }
+    }
+    database.set(userWeapon[0].owner, userWeapon)
+  }
+  
+  
+  transferItem(item, newOwner, oldOwner, database) {
+    /*
+    item = Weapon class
+    newOwner = user id (string)
+    oldOwner = user id (string)
+    database = the database table to set the new value
+    */
     
-    //use db.get() to fill the userWeapon
-    item = userWeapon.filter(i => i.id == item.id)
-    item[0].maxSkillSize += 1
-    database.set(item[0].owner, item)
+    //removing the weapon from old owner
+    let oldCollection = database.get(oldOwner)
+    oldCollection.filter(i => i.id !== item.id)
+    database.set(oldOwner, oldCollection)
+    
+    //adding the weeapon to new owner
+    let newCollection = database.get(newOwner)
+    item.ownerHistory.push(oldOwner)
+    newCollection.push(item)
+    database.set(newOwner, newCollection)
   }
   
 }
