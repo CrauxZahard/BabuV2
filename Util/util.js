9f60b5e84fb0a1d125cbfc46ea43103500be2b27
Craux <83466517+CrauxZahard@users.noreply.github.com>
2021-08-08 16:12:30 +0700

    Update util.js

--- Util/util.js
+++ Util/util.js
@@ -11,13 +11,18 @@ class Util {
       result += random.charAt(Math.floor(Math.random() * random.length))
     }
     
-    while(this.IDexist(result, database)) {
+    let tries = 0
+    while(this.IDexist(result, database) && tries <= 20) {
+      //misal udah ngulangin 20 kali dan tetap ada id yg sama, loop ini berenti
+      if(tries > 19) return false
+      
       //generate kodenya lagi
       let newCode = []
       for(var i = 0; i < amount; i++) {
        newCode[i] = random.charAt(Math.floor(Math.random() * random.length))
       }
       result = newCode.join('')
+      tries++
     }
     return result
   }
