b83e0063ded57d4184b4e64bedcacd94592efd4e
Craux <83466517+CrauxZahard@users.noreply.github.com>
2021-08-18 19:06:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const { API } = require('nhentai');
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES], partials: ['MESSAGE', 'REACTION', 'CHANNEL'] });
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS], partials: ['MESSAGE', 'REACTION', 'CHANNEL'] });
 const { token } = require('./token.json');
 client.db = require('quick.db');
 let hentai = require('./Util/hentai.js');
