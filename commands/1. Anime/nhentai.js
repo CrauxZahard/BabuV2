6c84a5156a285462dc8622f11ff39f5eec9e0c96
Craux <83466517+CrauxZahard@users.noreply.github.com>
2021-08-21 19:57:30 +0700

    Update nhentai.js

--- commands/1. Anime/nhentai.js
+++ commands/1. Anime/nhentai.js
@@ -1,6 +1,17 @@
 module.exports.name = "nhentai"
 module.exports.aliases = ["doujin", "nh", "search", "s"]
 module.exports.cooldown = 10
+module.exports.options = [{
+  name: 'Query',
+  type: 3,
+  description: 'a name of character/series you want to search.',
+  required: false
+}, {
+  name: 'Code',
+  type: 4,
+  description: 'a spesific "nuclear code" to search.',
+  required: false
+}]
 
 module.exports.code = async (client, message, args) => {
   let number = parseInt(args[0])
