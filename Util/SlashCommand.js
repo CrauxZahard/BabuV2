3eb45745f0658d1ef64d75ef3d7b06c640716556
Craux <83466517+CrauxZahard@users.noreply.github.com>
2021-08-21 19:10:42 +0700

    attempt to migrate into slash command

---
+++ Util/SlashCommand.js
@@ -0,0 +1,50 @@
+const axios = require('axios')
+const { token } = require('./token.json')
+class SlashCommand {
+  constructor(client) {
+    this.client = client
+  }
+  
+  addCommand(data) {
+    let commandData = {
+      name: data.name,
+      type: 1,
+      description: data.description,
+      options: data.options
+    }
+    
+    axios.post('https://discord.com/api/v8/applications/866931644983148554/commands', commandData, {
+     headers: {
+       Authorization: `Bot ${token}`
+     }
+    })
+    .then(res => {
+      console.log(res)
+      return res
+    })
+    .catch(err => {
+      console.log(err)
+      return;
+    })
+    
+  }
+  
+  updateCommand(commandId, newData) {
+    axios.patch(`https://discord.com/api/v8/applications/866931644983148554/commands/${commandId}`, newData, {
+    headers: {
+      Authorization: `Bot ${token}`
+    }
+    })
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+  }
+  
+  deleteCommand(commandId) {
+    axios.delete(`https://discord.com/api/v8/applications/866931644983148554/commands/${commandId}`, {
+      headers: {
+        Authorization: `Bot ${token}`
+      }
+    })
+  }
+  
+}
